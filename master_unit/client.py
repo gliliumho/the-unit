70d79df1ac14b374357a07b46e1da7b87e4f5358
Kin Yee Ho <kyho@recogine.com>
2016-01-26 18:57:48 +0800

    Add infinite loop to pull data from RT server

--- master_unit/client.py
+++ master_unit/client.py
@@ -1,9 +1,26 @@
 import socket
 
-s = socket.socket()
-host = socket.gethostbyname(socket.getfqdn())
-port = 20203
+# host = socket.gethostbyname(socket.getfqdn())
+# port = 20203
+host = '172.17.13.105'
+port = 9002
 
+
+s = socket.socket()
 s.connect((host, port))
-print(s.recv(2048).decode('utf-8'))
+print("Connected to "+host+":"+str(port))
+# s.settimeout(30)
+i = 0
+buffered = 0
+
+while True:
+    print("Waiting for data from RT server...")
+    data = s.recv(4096)
+
+    buffered = len(data)
+    print( str(i) + " -- " + str(buffered))
+    print( data.decode('utf-8') )
+    print("===="*20)
+    i += 1
+
 s.close()
